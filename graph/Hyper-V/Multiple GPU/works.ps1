573ec3a63a2d1716f28564699fa49bd3175e465a
Twin-Flame <61005029+Twin-Flame@users.noreply.github.com>
2021-09-15 18:41:10 -0400

    Update works.ps1

--- graph/Hyper-V/Multiple GPU/works.ps1
+++ graph/Hyper-V/Multiple GPU/works.ps1
@@ -18,7 +18,7 @@ $vm2 = "WindowsMiner"
 $gpu2 = "\\?\PCI#VEN_10DE&DEV_1F06&SUBSYS_87181043&REV_A1#4&296eb0ef&0&0008#{064092b3-625e-43bf-9eb5-dc845897dd59}\GPUPARAV"
 
 Remove-VMGpuPartitionAdapter -VMName $vm2
-Set-VMPartitionableGpu -Name $gpu1 -PartitionCount 1
+Set-VMPartitionableGpu -Name $gpu2 -PartitionCount 1
 Set-VM -GuestControlledCacheTypes $true -VMName $vm2
 Set-VM -LowMemoryMappedIoSpace 3GB -VMName $vm2
 Set-VM -HighMemoryMappedIoSpace 32GB -VMName $vm2
