80c1850957a525203f6c1ff5b55f7e83820f3145
Twin-Flame <61005029+Twin-Flame@users.noreply.github.com>
2021-09-15 19:40:08 -0400

    Update works.ps1

--- graph/Hyper-V/Multiple GPU/works.ps1
+++ graph/Hyper-V/Multiple GPU/works.ps1
@@ -24,3 +24,16 @@ Set-VM -LowMemoryMappedIoSpace 3GB -VMName $vm2
 Set-VM -HighMemoryMappedIoSpace 32GB -VMName $vm2
 Add-VMGpuPartitionAdapter -VMName $vm2
 Start-VM -Name $vm2
+
+# Nvidia GTX 1080 3GB
+$vm3 = "WindowsMiner"
+
+$gpu3 = "\\?\PCI#VEN_10DE&DEV_1C02&SUBSYS_61623842&REV_A1#4&2c577141&0&00E4#{064092b3-625e-43bf-9eb5-dc845897dd59}\GPUPARAV"
+
+Remove-VMGpuPartitionAdapter -VMName $vm3
+Set-VMPartitionableGpu -Name $gpu3 -PartitionCount 1
+Set-VM -GuestControlledCacheTypes $true -VMName $vm3
+Set-VM -LowMemoryMappedIoSpace 3GB -VMName $vm3
+Set-VM -HighMemoryMappedIoSpace 32GB -VMName $vm3
+Add-VMGpuPartitionAdapter -VMName $vm3
+Start-VM -Name $vm3
