2519b0338774e1a9ba3322d7188922bcdf8ca6ad
Twin-Flame <61005029+Twin-Flame@users.noreply.github.com>
2021-09-16 19:31:19 -0400

    Update and rename graph/Hyper-V/Multiple GPU/PartitionCount/2 GPUs - works.ps1 to graph/Hyper-V/Multiple GPU/GPU Per vm/Add Sequentialy.ps1

--- graph/Hyper-V/Multiple GPU/GPU Per vm/Add Sequentialy.ps1
+++ graph/Hyper-V/Multiple GPU/GPU Per vm/Add Sequentialy.ps1
@@ -4,10 +4,9 @@ $vm2 = "WindowsMiner"
 $gpu1 = "\\?\PCI#VEN_10DE&DEV_1C02&SUBSYS_61623842&REV_A1#4&2c577141&0&00E4#{064092b3-625e-43bf-9eb5-dc845897dd59}\GPUPARAV"
 $gpu2 = "\\?\PCI#VEN_10DE&DEV_1C03&SUBSYS_32811462&REV_A1#4&296eb0ef&0&0008#{064092b3-625e-43bf-9eb5-dc845897dd59}\GPUPARAV"
 
-# Solution:
-## Incrementing "PartitionCount" is the Solution!!
-Set-VMPartitionableGpu -Name $gpu1 -PartitionCount 1
-Set-VMPartitionableGpu -Name $gpu2 -PartitionCount 2
+
+Set-VMPartitionableGpu -Name $gpu1 -PartitionCount 10
+Set-VMPartitionableGpu -Name $gpu2 -PartitionCount 10
 
 
 # Set Instances
